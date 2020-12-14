3162793f46e63a0f4ecd6f288f27633d598bb4f8
uzaysan <alioksuz1126@gmail.com>
2020-12-14 22:50:19 +0300

    init

--- app/src/main/java/com/sonata/socialapp/activities/sonata/StartActivity.java
+++ app/src/main/java/com/sonata/socialapp/activities/sonata/StartActivity.java
@@ -49,11 +49,6 @@ public class StartActivity extends AppCompatActivity {
                     finish();
                 }
             });
-
-
-
-
-
         }
         else{
             startActivity(new Intent(this,LoginActivity.class));
